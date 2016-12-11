53f109d976ba080cf5a00b9f011a42bc92392173
Calvin Bui <c@lvin.email>
2016-12-11 19:52:09 +1100

    Changed Google Analytics library to ga-lite

---
+++ _includes/analytics.js
@@ -0,0 +1,5 @@
+<script src="https://cdn.jsdelivr.net/ga-lite/latest/ga-lite.min.js" async></script>
+<script>
+var galite = galite || {};
+galite.UA = 'UA-33090338-1'; // Insert your tracking code here
+</script>
