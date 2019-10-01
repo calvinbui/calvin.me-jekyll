dd2a3c9364cc9dff3f1a1054068388241ad85947
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2019-10-01 20:34:33 +1000

    fix critical css

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,12 @@ var critical = require('critical').stream;
 // have jekyll generate a site first
 // then run gulp
 gulp.task('default', function () {
-  return gulp.src('index.html') // HTML source file
+  return gulp
+    .src('_site/index.html') // HTML source file
     .pipe(critical({
-      base: '_site', // Base directory
-      css: ['_site/style.css'], // Your CSS Files
-      minify: true // Minify critical-path CSS
+        base: '_site', // Base directory
+        css: ['_site/style.css'], // Your CSS Files
+        minify: false // Minify critical-path CSS
     }))
     .on('error', function(err) { gutil.log(gutil.colors.red(err.message)); })
     .pipe(rename('critical-css.css'))
